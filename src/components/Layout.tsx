a632f999117b2f1abae85a697536424ec9d860ca
Julian <thejulianjuko@gmail.com>
2026-03-24 14:41:00 +0100

    layout fix

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -9,14 +9,14 @@ const navItems = [
 
 export default function Layout() {
 	return (
-		<div className="flex h-full flex-col bg-[#f2f2f7] dark:bg-black safe-top safe-x">
+		<div className="flex h-full pt-12 flex-col bg-[#f2f2f7] dark:bg-black safe-top safe-x">
 			{/* Page content */}
 			<main className="flex-1 overflow-y-auto overscroll-none px-4 pt-4 pb-24">
 				<Outlet />
 			</main>
 
 			{/* Tab bar */}
-			<nav className="fixed inset-x-0 bottom-0 z-40 border-t border-gray-300/60 bg-[#f2f2f7]/80 backdrop-blur-xl dark:border-white/[0.08] dark:bg-black/80 safe-bottom safe-x">
+			<nav className="fixed inset-x-0 bottom-0 z-40 border-t pb-4 border-gray-300/60 bg-[#f2f2f7]/80 backdrop-blur-xl dark:border-white/[0.08] dark:bg-black/80 safe-bottom safe-x">
 				<ul className="mx-auto flex max-w-md justify-around">
 					{navItems.map(({ to, label, Icon }) => (
 						<li key={to}>
